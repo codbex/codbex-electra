9744ad28fd3d5973524bc55d35e433ac7e9d1317
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-01-16 14:09:37 +0200

    store tracking number

--- codbex-electra-ext-econt/shipping/label/print-controller.js
+++ codbex-electra-ext-econt/shipping/label/print-controller.js
@@ -1,18 +1,49 @@
+let angularHttp;
+
 const app = angular.module('templateApp', []);
 app.controller('templateContoller', function ($scope, $http, $sce) {
+    angularHttp = $http;
     $scope.shippingLabelUrl = $sce.trustAsResourceUrl("about:blank");
 
     const urlString = (window.location.href).toLowerCase();
     const url = new URL(urlString);
-
     const salesOrderId = url.searchParams.get("id");
+
     const labelURL = `/services/js/codbex-electra-ext-econt/api/SalesOrders.js/${salesOrderId}/shippingLabelURL`;
 
     $http.get(labelURL)
         .then(function (response) {
             $scope.shippingLabelURL = $sce.trustAsResourceUrl(response.data.url);
         });
+
 });
+
+function handleShippingLabelConfirmed(data) {
+    const salesOrderId = parseInt(data.orderData.num);
+    const url = `/services/js/codbex-electra/gen/api/SalesOrders/SalesOrder.js/${salesOrderId}`;
+
+    const shipmentStatus = data.shipmentStatus;;
+
+    angularHttp.get(url)
+        .then(function (response) {
+            const salesOrder = response.data;
+
+            salesOrder.Tracking = shipmentStatus.shipmentNumber;
+            salesOrder.DateModified = Date.now();
+            salesOrder.DateAdded = undefined;
+
+            angularHttp.put(url, salesOrder)
+                .then(function () {
+                    console.log(`Successfully updated sales order with id [${salesOrderId}].`);
+                    parent.parent.location.reload();
+                }, function (response) {
+                    console.error(`Failed to update sales order with id [${salesOrderId}]. Response: ${JSON.stringify(response)}`);
+                });
+
+        }, function (response) {
+            console.error(`Failed to get sales order with id [${salesOrderId}]. Response: ${JSON.stringify(response)}`);
+        });
+};
 
 $(function ($) {
     const empty__ = function (thingy) {
@@ -25,14 +56,11 @@ $(function ($) {
 
         const eventType = messageData['event'];
         switch (eventType) {
-            case 'cancel':
-                console.log("Processing cancel");
-                break;
             case 'confirm':
+                handleShippingLabelConfirmed(messageData);
                 if (messageData['printPdf'] === true && !empty__(messageData['shipmentStatus']['pdfURL'])) {
                     window.open(messageData['shipmentStatus']['pdfURL'], '_blank');
                 }
-
                 break;
         }
     });
