de941d02d120d83eca402d111b3d07ba76924332
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-02-01 15:54:03 +0200

    WIP: add OC Product DAO

---
+++ codbex-electra-opencart/dao/ProductDAO.mjs
@@ -0,0 +1,37 @@
+import { dao as daoApi } from "@dirigible/db";
+
+const CONFIG = {
+	table: "oc_product",
+	properties: [
+		{
+			id: true,
+			name: "id",
+			column: "product_id",
+			type: "INTEGER",
+			autoIncrement: true
+		},
+		{
+			name: "model",
+			column: "model",
+			type: "VARCHAR"
+		}
+	]
+};
+
+const LOGGER_NAME = "ProductDAO";
+
+export class ProductDAO {
+	openCartDAO;
+
+	constructor(dataSourceName) {
+		this.openCartDAO = daoApi.create(CONFIG, LOGGER_NAME, dataSourceName);
+	}
+
+	get(id) {
+		return this.openCartDAO.find(id);
+	}
+
+	list(settings) {
+		return this.openCartDAO.list(settings);
+	}
+}
--- codbex-electra-opencart/synch/outbound/products/merge-product-to-open-cart.mjs
+++ codbex-electra-opencart/synch/outbound/products/merge-product-to-open-cart.mjs
@@ -1,14 +1,24 @@
 import { getLogger } from "/codbex-electra/util/logger-util.mjs";
 import * as productDAO from "/codbex-electra/gen/dao/Products/Product";
-
+import { ProductDAO } from "/codbex-electra-opencart/dao/ProductDAO.mjs";
 const logger = getLogger(import.meta.url);
 
 export function onMessage(message) {
     const productEntry = message.getBody();
     const productId = productEntry.productId;
-    const product = getProduct(productId);
 
+    const product = getProduct(productId);
     logger.info("Received product entry [{}]", JSON.stringify(product));
+
+    const dataSourceName = productEntry.store.dataSourceName;
+    const ocProductDAO = new ProductDAO(dataSourceName);
+
+    const osProducts = ocProductDAO.list();
+    logger.info("osProducts[{}]", osProducts);
+
+
+    const osProduct = ocProductDAO.get(51);
+    logger.info("osProduct[{}]", osProduct);
     return message;
 }
 
