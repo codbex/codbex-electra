2ef8aa5a3928485e412d1b897e01524213fed3af
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-02-05 13:17:42 +0200

    add logs

--- codbex-electra-opencart/synch/outbound/products/get-store-products.mjs
+++ codbex-electra-opencart/synch/outbound/products/get-store-products.mjs
@@ -1,9 +1,16 @@
 import * as productToStoreDAO from "/codbex-electra/gen/dao/Products/ProductToStore";
 import * as entityReferenceDAO from "/codbex-electra/gen/dao/Settings/EntityReference";
+import { getLogger } from "/codbex-electra/util/logger-util.mjs";
+
+const logger = getLogger(import.meta.url);
+
 
 export function onMessage(message) {
     const store = message.getBody();
+
     const productToStoreEntries = getProductToStoreEntries(store.id);
+    logger.info("Found [{}] products which must be replicated to store [{}]", productToStoreEntries.length, store.name);
+
     const productReferences = getStoreProductReferences(store.id);
 
     const productEntries = [];
