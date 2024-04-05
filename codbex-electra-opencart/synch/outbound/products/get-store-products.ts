4df074e227a2919c2cc6069e5bd7aed423138688
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-04-05 11:09:46 +0300

    more imports
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- codbex-electra-opencart/synch/outbound/products/get-store-products.ts
+++ codbex-electra-opencart/synch/outbound/products/get-store-products.ts
@@ -1,4 +1,4 @@
-import { ProductToStoreRepository as ProductToStoreDAO, ProductDescriptionEntityOptions } from "codbex-electra/gen/dao/Products/ProductToStoreRepository";
+import { ProductToStoreRepository as ProductToStoreDAO, ProductToStoreEntityOptions } from "codbex-electra/gen/dao/Products/ProductToStoreRepository";
 import { OpenCartStoreConfig } from "codbex-electra-opencart/dao/StoreConfigDAO";
 import { BaseHandler } from "codbex-electra-opencart/synch/base-handler";
 
@@ -43,7 +43,7 @@ class GetStoreProductsHandler extends BaseHandler {
     }
 
     private getStoreProductIds() {
-        const querySettings: ProductDescriptionEntityOptions = {
+        const querySettings: ProductToStoreEntityOptions = {
             $filter: {
                 equals: {
                     Store: this.store.id
