425579608baf49379f7ce7d74ff7c654344c061c
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-04-17 12:29:53 +0300

    fix perspective

--- codbex-electra-ext-econt/shipping/label/print.js
+++ codbex-electra-ext-econt/shipping/label/print.js
@@ -2,7 +2,7 @@ const viewData = {
     id: 'sales-order-print-shipping-label',
     label: 'Print Shipping Label',
     link: '/services/web/codbex-electra-ext-econt/shipping/label/print-index.html',
-    perspective: 'SalesOrders',
+    perspective: 'sales-orders',
     view: 'SalesOrder',
     type: 'entity',
     order: 1
