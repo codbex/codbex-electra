3275d0922b2c3ee9d5c7df76d4d0dc497a7ccc05
iliyan-velichkov <velichkov.iliyan@gmail.com>
2024-05-09 11:38:26 +0300

    increase/decresease product quantity on completed order and reopened order

--- codbex-electra/listeners/sales-orders-handler.ts
+++ codbex-electra/listeners/sales-orders-handler.ts
@@ -1,20 +1,99 @@
-import { SalesOrderRepository } from "codbex-electra/gen/dao/sales-orders/SalesOrderRepository";
+import { SalesOrderUpdateEntityEvent, SalesOrderEntity } from "codbex-electra/gen/dao/sales-orders/SalesOrderRepository";
+import { SalesOrderItemRepository as SalesOrderItemDAO, SalesOrderItemEntityOptions, SalesOrderItemEntity } from "codbex-electra/gen/dao/sales-orders/SalesOrderItemRepository";
+import { ProductRepository as ProductDAO } from "codbex-electra/gen/dao/products/ProductRepository";
+import { StoreConfigDAO } from "codbex-electra-opencart/dao/StoreConfigDAO";
 import { getLogger } from "codbex-electra/util/LoggerUtil";
 
 const logger = getLogger(import.meta.url);
 
-export function onMessage(message: string) {
-    logger.info("Listener: received message [{}]", message);
+function isInstanceOfSalesOrderUpdateEntityEvent(obj: any): obj is SalesOrderUpdateEntityEvent {
+    return obj.previousEntity !== undefined;
+}
 
-    const repo = new SalesOrderRepository();
-    const orders = repo.findAll();
-    logger.info("Listener: found [{}] sales orders. Sales orders: [{}]", orders.length, JSON.stringify(orders));
+export function onMessage(message: string) {
+    const event: any = JSON.parse(message);
+    if (!isInstanceOfSalesOrderUpdateEntityEvent(event)) {
+        logger.info("Event message [{}] is not applicable for this listener", message);
+        return;
+    }
+    const updateEvent: SalesOrderUpdateEntityEvent = event;
+    const currentEntity = updateEvent.entity;
+    const previousEntity = updateEvent.previousEntity;
+
+    const quantitiesUpdater = new QuantitiesUpdater();
+    quantitiesUpdater.updateQuantities(currentEntity, previousEntity);
 }
 
 export function onError(error: string) {
-    logger.info("Listener: received error [{}]", error);
+    // nothing to do here
+}
 
-    const repo = new SalesOrderRepository();
-    const orders = repo.findAll();
-    logger.info("Listener: found [{}] sales orders. Sales orders: [{}]", orders.length, JSON.stringify(orders));
+
+class QuantitiesUpdater {
+    protected readonly salesOrderItemDAO;
+    protected readonly storeConfigDAO;
+    protected readonly productDAO;
+
+    constructor() {
+        this.salesOrderItemDAO = new SalesOrderItemDAO();
+        this.storeConfigDAO = new StoreConfigDAO();
+        this.productDAO = new ProductDAO();
+    }
+
+    updateQuantities(currentOrder: Partial<SalesOrderEntity>, previousOrder: SalesOrderEntity) {
+        logger.info("About to update quantities. Current order [{}], Prev order [{}]", currentOrder, previousOrder);
+
+        const completeOrderStatusId = this.storeConfigDAO.getStoreCompleteOrderStatusId(previousOrder.Store);
+
+        const completedOrder = currentOrder.Status === completeOrderStatusId && previousOrder.Status !== completeOrderStatusId;
+        if (completedOrder) {
+            logger.info("Order [{}] is completed. Will reduce products quantity", previousOrder.Id);
+            this.reduceProductsQuantity(previousOrder.Id);
+            return;
+        }
+
+        const reopenedOrder = currentOrder.Status !== completeOrderStatusId && previousOrder.Status == completeOrderStatusId;
+        if (reopenedOrder) {
+            logger.info("Order [{}] is reopened. Will increase products quantity", previousOrder.Id);
+            this.increaseProductsQuantity(previousOrder.Id);
+            return;
+        }
+    }
+
+    private reduceProductsQuantity(orderId: number) {
+        const orderItems = this.getOrderItems(orderId);
+        orderItems.forEach(orderItem => {
+            const orderedQuantity = orderItem.Quantity;
+            const product = this.productDAO.findById(orderItem.Product)!;
+            logger.info("Reducing quantity [{}] with [{}] for product [{}]", product.Quantity, orderedQuantity, product.Id);
+            product.Quantity = product.Quantity - orderedQuantity;
+
+            this.productDAO.update(product);
+        });
+    }
+
+    private increaseProductsQuantity(orderId: number) {
+        const orderItems = this.getOrderItems(orderId);
+        orderItems.forEach(orderItem => {
+            const orderedQuantity = orderItem.Quantity;
+            const product = this.productDAO.findById(orderItem.Product)!;
+            logger.info("Increasing quantity [{}] with [{}] for product [{}]", product.Quantity, orderedQuantity, product.Id);
+            product.Quantity = product.Quantity + orderedQuantity;
+
+            this.productDAO.update(product);
+        });
+    }
+
+    private getOrderItems(orderId: number) {
+        const querySettings: SalesOrderItemEntityOptions = {
+            $select: ["Quantity", "Product"],
+            $filter: {
+                equals: {
+                    SalesOrder: orderId
+                }
+            }
+        };
+        return this.salesOrderItemDAO.findAll(querySettings);
+    }
+
 }
